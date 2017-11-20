b34248624341535ac18fa87b14cf593e7541eaf6
r.khavronenko <r.khavronenko@vertamedia.com>
2017-11-20 14:35:39 +0200

    rm unnecessary exports

--- dist/module.d.ts
+++ dist/module.d.ts
@@ -3,10 +3,4 @@ import { SqlQueryCtrl } from './query_ctrl';
 declare class SqlConfigCtrl {
     static templateUrl: string;
 }
-declare class SqlQueryOptionsCtrl {
-    static templateUrl: string;
-}
-declare class SqlAnnotationsQueryCtrl {
-    static templateUrl: string;
-}
-export { ClickHouseDatasource as Datasource, SqlQueryCtrl as QueryCtrl, SqlConfigCtrl as ConfigCtrl, SqlQueryOptionsCtrl as QueryOptionsCtrl, SqlAnnotationsQueryCtrl as AnnotationsQueryCtrl };
+export { ClickHouseDatasource as Datasource, SqlQueryCtrl as QueryCtrl, SqlConfigCtrl as ConfigCtrl };
--- dist/module.js
+++ dist/module.js
@@ -1,6 +1,6 @@
 System.register(['./datasource', './query_ctrl'], function(exports_1) {
     var datasource_1, query_ctrl_1;
-    var SqlConfigCtrl, SqlQueryOptionsCtrl, SqlAnnotationsQueryCtrl;
+    var SqlConfigCtrl;
     return {
         setters:[
             function (datasource_1_1) {
@@ -16,23 +16,9 @@ System.register(['./datasource', './query_ctrl'], function(exports_1) {
                 SqlConfigCtrl.templateUrl = 'partials/config.html';
                 return SqlConfigCtrl;
             })();
-            SqlQueryOptionsCtrl = (function () {
-                function SqlQueryOptionsCtrl() {
-                }
-                SqlQueryOptionsCtrl.templateUrl = 'partials/query.options.html';
-                return SqlQueryOptionsCtrl;
-            })();
-            SqlAnnotationsQueryCtrl = (function () {
-                function SqlAnnotationsQueryCtrl() {
-                }
-                SqlAnnotationsQueryCtrl.templateUrl = 'partials/annotations.editor.html';
-                return SqlAnnotationsQueryCtrl;
-            })();
             exports_1("Datasource", datasource_1.ClickHouseDatasource);
             exports_1("QueryCtrl", query_ctrl_1.SqlQueryCtrl);
             exports_1("ConfigCtrl", SqlConfigCtrl);
-            exports_1("QueryOptionsCtrl", SqlQueryOptionsCtrl);
-            exports_1("AnnotationsQueryCtrl", SqlAnnotationsQueryCtrl);
         }
     }
 });

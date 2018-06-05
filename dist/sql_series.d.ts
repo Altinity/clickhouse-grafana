dd43ce44b0a89a052db8bfc91f68b74f0c5a5e86
Marek Vavruša <mvavrusa@cloudflare.com>
2018-06-04 21:45:07 -0700

    sql_series: optimize memory use for range time series
    Refactored the series parsing code to avoid intermediate object allocations
    and mutations, which make longer time series too expensive. The series
    is now constructed in single pass, backfilling null values when needed.

--- dist/sql_series.d.ts
+++ dist/sql_series.d.ts
@@ -10,6 +10,7 @@ export default class SqlSeries {
     toTable(): any;
     toTimeSeries(): any;
     extrapolate(datapoints: any): any;
+    static _pushDatapoint(metrics: any, timestamp: number, key: string, value: number): void;
     static _toJSType(type: any): string;
     static _formatValue(value: any): any;
 }

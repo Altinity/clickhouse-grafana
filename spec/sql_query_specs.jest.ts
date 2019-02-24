017b8e46273e85f1d76e03d4f48242f85dbf981f
hagen1778 <hagen1778@gmail.com>
2019-02-24 10:08:17 +0000

    format query specs

--- spec/sql_query_specs.jest.ts
+++ spec/sql_query_specs.jest.ts
@@ -11,7 +11,8 @@ describe("Query SELECT with $timeFilterByColumn and range with from and to:", ()
   };
 
   it("gets replaced with BETWEEN filter", () => {
-    expect(SqlQuery.replaceTimeFilters(query, range, 'DATETIME')).toBe('SELECT * FROM table WHERE column_name BETWEEN toDateTime(1545613323) AND toDateTime(1546300799)');
+    expect(SqlQuery.replaceTimeFilters(query, range, 'DATETIME'))
+        .toBe('SELECT * FROM table WHERE column_name BETWEEN toDateTime(1545613323) AND toDateTime(1546300799)');
   });
 });
 
@@ -27,13 +28,20 @@ describe("Query SELECT with $timeFilterByColumn and range with from", () => {
   };
 
   it("gets replaced with >= filter", () => {
-    expect(SqlQuery.replaceTimeFilters(query, range, 'DATETIME')).toBe('SELECT * FROM table WHERE column_name >= toDateTime(1545613323)');
+    expect(SqlQuery.replaceTimeFilters(query, range, 'DATETIME')).
+    toBe('SELECT * FROM table WHERE column_name >= toDateTime(1545613323)');
   });
 });
 
 describe("$unescape", () => {
-    const query = "SELECT $unescape('count()'), $unescape('if(runningDifference(max_0) < 0, nan, runningDifference(max_0) / runningDifference(t/1000)) AS max_0_Rate') FROM requests WHERE $unescape('client_ID') = 5";
+    const query = "SELECT $unescape('count()'), " +
+        "$unescape('if(runningDifference(max_0) < 0, nan, " +
+        "runningDifference(max_0) / runningDifference(t/1000)) AS max_0_Rate') " +
+        "FROM requests WHERE $unescape('client_ID') = 5";
+    const expQuery = "SELECT count(), if(runningDifference(max_0) < 0, " +
+        "nan, runningDifference(max_0) / runningDifference(t/1000)) AS max_0_Rate " +
+        "FROM requests WHERE client_ID = 5";
     it("gets replaced with >= filter", () => {
-        expect(SqlQuery.unescape(query)).toBe('SELECT count(), if(runningDifference(max_0) < 0, nan, runningDifference(max_0) / runningDifference(t/1000)) AS max_0_Rate FROM requests WHERE client_ID = 5');
+        expect(SqlQuery.unescape(query)).toBe(expQuery);
     });
 });

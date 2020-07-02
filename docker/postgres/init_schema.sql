73cccf547f61a6b8dff0c1b510c348c2c4876181
Eugene Klimov <eklimov@altinity.com>
2020-07-02 20:49:54 +0500

    cleanup postgres init_schema.sql and return to grafana 7.x in docker-compose.yaml
    Signed-off-by: Eugene Klimov <eklimov@altinity.com>

--- docker/postgres/init_schema.sql
+++ docker/postgres/init_schema.sql
@@ -1,9 +1,7 @@
 CREATE USER grafana WITH PASSWORD 'grafana';
 CREATE DATABASE grafana;
+ALTER DATABASE grafana OWNER TO grafana;
 GRANT ALL PRIVILEGES ON DATABASE grafana TO grafana;
-
-ALTER DEFAULT PRIVILEGES GRANT ALL ON TABLES TO grafana;
-ALTER DEFAULT PRIVILEGES GRANT ALL ON SEQUENCES TO grafana;
 
 \c grafana
 
@@ -15,5 +13,3 @@ INSERT INTO test_grafana(event_time, service_name, too_big_value) SELECT to_time
 INSERT INTO test_grafana(event_time, service_name, too_big_value) SELECT to_timestamp(extract(epoch from now()) + ((500+generate_series)*10)) AS event_time, 'postgresql' AS service_name, 1000000000.05 AS too_big_value FROM generate_series(1,1000);
 
 ALTER TABLE test_grafana OWNER TO grafana;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO grafana;
-GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO grafana;

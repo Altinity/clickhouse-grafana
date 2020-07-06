6e66ef70cf9623bef4c19804709ca3fd901789b0
Eugene Klimov <eklimov@altinity.com>
2020-07-06 16:52:00 +0500

    - add backend alerting support for Windows and MacOSX, see https://github.com/grafana/grafana-plugin-repository/pull/656#issuecomment-654126434 - resolve all grammarly issues on README.md - add release.sh script for semi-automation make release process - add datetime_table_dashboard.json for illustrate "override" options with custom Unit `time: YYYY-MM-DD HH:mm:ss`, see https://github.com/grafana/grafana/issues/25884 for explanation
    Signed-off-by: Eugene Klimov <eklimov@altinity.com>

---
+++ release.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -xeuo pipefail
+if [[ $# -lt 1 ]]; then
+    echo "release.sh [major|minor|patch]"
+    exit 1
+fi
+echo 1 > /proc/sys/vm/drop_caches
+source .release_env
+git config core.eol lf
+git config core.autocrlf input
+git config user.name "$GITHUB_LOGIN"
+git config user.email "$GITHUB_EMAIL"
+docker-compose down
+docker-compose run frontend_builder
+docker-compose run backend_builder
+bump2version --verbose $1
+git push

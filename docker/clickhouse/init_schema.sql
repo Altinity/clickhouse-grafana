93fedff7b4bcd58dbf392dff4a0cd2f414404a95
Eugene Klimov <eklimov@altinity.com>
2020-07-07 15:09:36 +0500

    add country codes to default.test_grafana, resolve sql_series.ts TSLint issues, close https://github.com/Vertamedia/clickhouse-grafana/issues/124
    Signed-off-by: Eugene Klimov <eklimov@altinity.com>

--- docker/clickhouse/init_schema.sql
+++ docker/clickhouse/init_schema.sql
@@ -3,15 +3,16 @@ CREATE TABLE IF NOT EXISTS default.test_grafana
 (
     event_time    DateTime,
     service_name  LowCardinality(String),
+    country       LowCardinality(String),
     too_big_value Float64
 )
     ENGINE = MergeTree()
         PARTITION BY toYYYYMM(event_time)
         ORDER BY (event_time, service_name);
 
-INSERT INTO default.test_grafana(event_time, service_name, too_big_value) SELECT toDateTime(now()-(number*10)) AS event_time, if(rand() % 2 = 1,'mysql','postgresql') AS service_name, 1000000000.05 AS too_big_value FROM numbers(1000);
-INSERT INTO default.test_grafana(event_time, service_name, too_big_value) SELECT toDateTime(now()+(number*10)) AS event_time, 'mysql' AS service_name, 1000000000.05 AS too_big_value FROM numbers(1000);
-INSERT INTO default.test_grafana(event_time, service_name, too_big_value) SELECT toDateTime(now()+((500+number)*10)) AS event_time, 'postgresql' AS service_name, 1000000000.05 AS too_big_value FROM numbers(1000);
+INSERT INTO default.test_grafana(event_time, service_name, country, too_big_value) SELECT toDateTime(now()-(number*10)) AS event_time, if(rand() % 2 = 1,'mysql','postgresql') AS service_name, multiIf(rand() % 10= 1,'RU', rand() % 10= 2,'DE', rand() % 10= 3,'CN', rand() % 10= 4,'UK', rand() % 10= 5,'NL', rand() % 10= 6,'EU', rand() % 10= 7,'TK', rand() % 10= 8,'AR', rand() % 10= 9,'FR', 'US') AS country, 1000000000.05 AS too_big_value FROM numbers(1000);
+INSERT INTO default.test_grafana(event_time, service_name, country, too_big_value) SELECT toDateTime(now()+(number*10)) AS event_time, 'mysql' AS service_name, , multiIf(rand() % 10= 1,'RU', rand() % 10= 2,'DE', rand() % 10= 3,'CN', rand() % 10= 4,'UK', rand() % 10= 5,'NL', rand() % 10= 6,'EU', rand() % 10= 7,'TK', rand() % 10= 8,'AR', rand() % 10= 9,'FR', 'US') AS country, 1000000000.05 AS too_big_value FROM numbers(1000);
+INSERT INTO default.test_grafana(event_time, service_name, country, too_big_value) SELECT toDateTime(now()+((500+number)*10)) AS event_time, 'postgresql' AS service_name, , multiIf(rand() % 10= 1,'RU', rand() % 10= 2,'DE', rand() % 10= 3,'CN', rand() % 10= 4,'UK', rand() % 10= 5,'NL', rand() % 10= 6,'EU', rand() % 10= 7,'TK', rand() % 10= 8,'AR', rand() % 10= 9,'FR', 'US') AS country, 1000000000.05 AS too_big_value FROM numbers(1000);
 
 
 CREATE TABLE default.test_alerts

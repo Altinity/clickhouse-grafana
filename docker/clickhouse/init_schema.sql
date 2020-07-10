2b8124526d7fa83eb17060a60d3925ef0c81d549
Eugene Klimov <eklimov@altinity.com>
2020-07-10 18:53:24 +0500

    reproduce and close https://github.com/Vertamedia/clickhouse-grafana/issues/232, fix tsLint issues on sql_query.ts
    Signed-off-by: Eugene Klimov <eklimov@altinity.com>

--- docker/clickhouse/init_schema.sql
+++ docker/clickhouse/init_schema.sql
@@ -29,3 +29,17 @@ CREATE TABLE IF NOT EXISTS default.test_alerts
 -- INSERT INTO default.test_alerts SELECT concat('test',toString(rand() % 10)) AS Name, toDate(now()) AS EventDate, now() AS EventTime, rand() AS Value FROM numbers(1000);
 
 INSERT INTO default.test_alerts VALUES  ('test1', toDate(now()), now(), 21);
+
+DROP TABLE IF EXISTS default.test_depends_on_variable;
+CREATE TABLE IF NOT EXISTS default.test_depends_on_variable(
+    event_time DateTime,
+    bulk_id LowCardinality(String),
+    city LowCardinality(Nullable(String)),
+    service_name LowCardinality(String),
+    too_big_value UInt64
+)
+    ENGINE = MergeTree()
+        PARTITION BY toYYYYMM(event_time)
+        ORDER BY (event_time, bulk_id, city, service_name);
+
+INSERT INTO default.test_depends_on_variable(event_time, bulk_id, city, service_name, too_big_value) SELECT toDateTime(now()-(number*10)) AS event_time, concat('bulk',toString(number%10)) AS bulk_id, if (number%600 > 0,concat('city',toString(number%600)),null) AS city, concat('service',toString(number%1000)) AS service_name, rand64() AS too_big_value FROM numbers(10000);

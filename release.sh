022bbbae82f5d672282acefd6063af57b90d4666
Eugene Klimov <eklimov@altinity.com>
2024-04-10 12:15:46 +0500

    upgrade to node 20 and golang 1.22 in release.yml rollback version to 3.0.0 in package.json

--- release.sh
+++ release.sh
@@ -9,16 +9,14 @@ git config core.eol lf
 git config core.autocrlf input
 git config user.name "$GITHUB_LOGIN"
 git config user.email "$GITHUB_EMAIL"
+bump2version --verbose $1
 docker-compose stop
 docker-compose run --rm frontend_builder
 docker-compose run --rm backend_builder
 dos2unix ./dist/*
 chmod +rx ./dist
 chmod +rx -R ./dist/altinity-clickhouse-plugin*
-git add .
-git diff-index --quiet HEAD || git commit -m "prepare to new release, $(grep current_version .bumpversion.cfg)"
-bump2version --verbose $1
 docker-compose run --rm plugin_signer
 git add .
-git commit -s -m "sign plugin, $(grep current_version .bumpversion.cfg)"
+git diff-index --quiet HEAD || git commit -s -m "prepare to new release, $(grep current_version .bumpversion.cfg)"
 git push --tags

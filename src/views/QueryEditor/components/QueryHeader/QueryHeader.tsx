239d0e50bd2d15770e3d6ae1618911d553b8067d
Roman M <roman.misyurin@gmail.com>
2024-05-29 18:38:15 +0200

    Add correct empty strings display

--- src/views/QueryEditor/components/QueryHeader/QueryHeader.tsx
+++ src/views/QueryEditor/components/QueryHeader/QueryHeader.tsx
@@ -11,25 +11,33 @@ function findDifferences(query: CHQuery, datasource: CHDataSource) {
 
   const differences: any[] = [];
 
+  function checkValue(value) {
+    if (value === undefined || value.trim() === '') {
+      return 'EMPTY';
+    } else {
+      return value.trim();
+    }
+  }
+
   if (defaultValues) {
     if (query.dateTimeType !== defaultValues.defaultDateTimeType) {
-      differences.push({ key: 'Timestamp type Column', original: query.dateTimeType || 'EMPTY', updated: defaultValues.defaultDateTimeType, fieldName: 'dateTimeType'});
+      differences.push({ key: 'Timestamp type Column', original: checkValue(query.dateTimeType), updated: defaultValues.defaultDateTimeType, fieldName: 'dateTimeType'});
     }
 
     if ((defaultValues.defaultDateTimeType === 'TIMESTAMP') && defaultValues.dateTime.defaultUint32 && query.dateTimeColDataType !== defaultValues.dateTime.defaultUint32) {
-      differences.push({ key: 'Timestamp Column', original: String(query.dateTimeColDataType).trim() || 'EMPTY', updated: defaultValues.dateTime.defaultUint32, fieldName: 'dateTimeColDataType' });
+      differences.push({ key: 'Timestamp Column', original: checkValue(query.dateTimeColDataType), updated: defaultValues.dateTime.defaultUint32, fieldName: 'dateTimeColDataType' });
     }
 
     if ((defaultValues.defaultDateTimeType === TimestampFormat.DateTime64) && defaultValues.dateTime.defaultDateTime64 && query.dateTimeColDataType !== defaultValues.dateTime.defaultDateTime64) {
-      differences.push({ key: 'Timestamp Column', original: String(query.dateTimeColDataType).trim() || 'EMPTY', updated: defaultValues.dateTime.defaultDateTime64, fieldName: 'dateTimeColDataType' });
+      differences.push({ key: 'Timestamp Column', original: checkValue(query.dateTimeColDataType), updated: defaultValues.dateTime.defaultDateTime64, fieldName: 'dateTimeColDataType' });
     }
 
     if ((defaultValues.defaultDateTimeType === TimestampFormat.DateTime) && defaultValues.dateTime.defaultDateTime && query.dateTimeColDataType !== defaultValues.dateTime.defaultDateTime) {
-      differences.push({ key: 'Timestamp Column', original: String(query.dateTimeColDataType).trim() || 'EMPTY', updated: defaultValues.dateTime.defaultDateTime, fieldName: 'dateTimeColDataType' });
+      differences.push({ key: 'Timestamp Column', original: checkValue(query.dateTimeColDataType), updated: defaultValues.dateTime.defaultDateTime, fieldName: 'dateTimeColDataType' });
     }
 
     if (defaultValues.dateTime.defaultDateDate32 && query.dateColDataType !== defaultValues.dateTime.defaultDateDate32) {
-      differences.push({ key: 'Date column', original: String(query.dateColDataType).trim() || 'EMPTY', updated: defaultValues.dateTime.defaultDateDate32, fieldName: 'dateColDataType' });
+      differences.push({ key: 'Date column', original: checkValue(query.dateColDataType), updated: defaultValues.dateTime.defaultDateDate32, fieldName: 'dateColDataType' });
     }
 
   }

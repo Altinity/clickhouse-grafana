2d8c03c90dbb3d77c20bc56f7d3945588e12176c
antip00 <antipov@altinity.com>
2024-07-17 16:00:36 +0300

    Updating create-report.sh.

--- tests/testflows/infra/create-report.sh
+++ tests/testflows/infra/create-report.sh
@@ -4,7 +4,7 @@ export PYTHONIOENCODING=utf-8
 mkdir Assets
 cp -r  tests/testflows/tmp/target/ ./Assets
 mkdir Reports
-cp raw.log  ./Reports/raw.log
+cp tests/testflows/raw.log  ./Reports/raw.log
 tfs --debug --no-colors transform compact raw.log > ./Reports/compact.log
 tfs --debug --no-colors transform nice raw.log   > ./Reports/nice.log.txt
 tfs --debug --no-colors report results -a "$CI_JOB_URL/artifacts/browse" raw.log - --confidential --copyright "Altinity Inc" | tfs --debug --no-colors document convert > ./Reports/result.html

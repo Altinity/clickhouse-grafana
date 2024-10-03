df4ac85e706d95d4b7a285b29bef2b86f3387e64
Eugene Klimov <eklimov@altinity.com>
2024-10-03 14:42:12 +0400

    fix alert message in data source for default values

--- src/views/ConfigEditor/FormParts/DefaultValues/DefaultValues.tsx
+++ src/views/ConfigEditor/FormParts/DefaultValues/DefaultValues.tsx
@@ -98,154 +98,138 @@ jsonData, newOptions, onSwitchToggle, onFieldChange, externalProps
     doRequest()
   }, [jsonData.dataSourceUrl, jsonData.useDefaultConfiguration, newOptions]);
 
-  return <div className="gf-form-group">
-    <InlineField
-      label="Use default values"
-      labelWidth={36}
-    >
-      <InlineSwitch
-        id="useDefaultConfiguration"
-        className="gf-form"
-        value={jsonData.useDefaultConfiguration || false}
-        onChange={(e) => onSwitchToggle('useDefaultConfiguration', e.currentTarget.checked)}
-      />
-    </InlineField>
-    {jsonData.useDefaultConfiguration && <>
-    {newOptions.version === 1 &&
-      <Alert
-        title={`Please save dashboard before use default configurations, 
+  return (
+    <div className="gf-form-group">
+      <InlineField label="Use default values" labelWidth={36}>
+        <InlineSwitch
+          id="useDefaultConfiguration"
+          className="gf-form"
+          value={jsonData.useDefaultConfiguration || false}
+          onChange={(e) => onSwitchToggle('useDefaultConfiguration', e.currentTarget.checked)}
+        />
+      </InlineField>
+      {jsonData.useDefaultConfiguration && (
+        <>
+          {newOptions.version === 1 && (
+            <Alert
+              title={`Please save data source before use default configurations, 
         we need configured clickhouse connection to fetch options`}
-        severity={'info'}
-        key={'info'}/>
-    }
-      <h6>TimestampType</h6>
-      <InlineField
-        labelWidth={32}
-        style={{marginLeft: '30px'}}
-        label={
-          <InlineLabel
-            width={32}
-            tooltip={
-              <div style={{ width: '200px', backgroundColor: 'black' }}>
-                Select Type &nbsp;
-                <a
-                  href="https://clickhouse.com/docs/en/sql-reference/data-types/datetime/"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  DateTime
-                </a>
-                ,&nbsp;
-                <a
-                  href="https://clickhouse.com/docs/en/sql-reference/data-types/datetime64/"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  DateTime64
-                </a>
-                &nbsp; or{' '}
-                <a
-                  href="https://clickhouse.com/docs/en/sql-reference/data-types/int-uint/"
-                  rel="noreferrer"
-                  target="_blank"
-                >
-                  UInt32
-                </a>{' '}
-                column for binding with Grafana range selector
-              </div>
+              severity={'info'}
+              key={'info'}
+            />
+          )}
+          <h6>TimestampType</h6>
+          <InlineField
+            labelWidth={32}
+            style={{ marginLeft: '30px' }}
+            label={
+              <InlineLabel
+                width={32}
+                tooltip={
+                  <div style={{ width: '200px', backgroundColor: 'black' }}>
+                    Select Type &nbsp;
+                    <a
+                      href="https://clickhouse.com/docs/en/sql-reference/data-types/datetime/"
+                      rel="noreferrer"
+                      target="_blank"
+                    >
+                      DateTime
+                    </a>
+                    ,&nbsp;
+                    <a
+                      href="https://clickhouse.com/docs/en/sql-reference/data-types/datetime64/"
+                      rel="noreferrer"
+                      target="_blank"
+                    >
+                      DateTime64
+                    </a>
+                    &nbsp; or{' '}
+                    <a
+                      href="https://clickhouse.com/docs/en/sql-reference/data-types/int-uint/"
+                      rel="noreferrer"
+                      target="_blank"
+                    >
+                      UInt32
+                    </a>{' '}
+                    column for binding with Grafana range selector
+                  </div>
+                }
+              >
+                Column timestamp type
+              </InlineLabel>
             }
-
           >
-            Column timestamp type
-          </InlineLabel>
-        }
-
-      >
-        <Select
-          width={24}
-          onChange={(changeEvent) => {
-            onFieldChange({ value: changeEvent ? changeEvent.value : undefined }, 'defaultDateTimeType')
-          }}
-          isClearable
-          placeholder={'Timestamp type'}
-          options={[
-            { label: 'DateTime', value: TimestampFormat.DateTime },
-            { label: 'DateTime64', value: TimestampFormat.DateTime64 },
-            { label: 'TimeStamp', value: TimestampFormat.TimeStamp },
-          ]}
-          value={jsonData.defaultDateTimeType}
-        />
-      </InlineField>
-      <h6>DateTime columns</h6>
-      <InlineField
-        label="Datetime Field"
-        labelWidth={32}
-        style={{marginLeft: '30px'}}
-      >
-        <Select
-          isClearable
-          id="defaultDateTime"
-          allowCustomValue={false}
-          width={24}
-          value={jsonData.defaultDateTime}
-          onChange={(changeEvent) => {
-            onFieldChange({ value: changeEvent ? changeEvent.value : undefined }, 'defaultDateTime')
-          }}
-          options={defaultDateTimeOptions}
-        />
-      </InlineField>
-      <InlineField
-        label="Timestamp (Uint32) Field"
-        labelWidth={32}
-        style={{marginLeft: '30px'}}
-      >
-        <Select
-          isClearable
-          id="defaultUint32"
-          allowCustomValue={false}
-          width={24}
-          value={jsonData.defaultUint32}
-          onChange={(changeEvent) => {
-            onFieldChange({ value: changeEvent ? changeEvent.value : undefined }, 'defaultUint32')
-          }}
-          options={defaultUint32Options}
-        />
-      </InlineField>
-      <InlineField
-        label="Datetime64 Field"
-        labelWidth={32}
-        style={{marginLeft: '30px'}}
-      >
-        <Select
-          isClearable
-          id="defaultDateTime64"
-          allowCustomValue={false}
-          width={24}
-          value={jsonData.defaultDateTime64}
-          onChange={(changeEvent) => {
-            onFieldChange({ value: changeEvent ? changeEvent.value : undefined }, 'defaultDateTime64')
-          }}
-          options={defaultDateTime64Options}
-        />
-      </InlineField>
-      <h6>Date column</h6>
-      <InlineField
-        label="Date Field"
-        labelWidth={32}
-        style={{marginLeft: '30px'}}
-      >
-        <Select
-          isClearable
-          id="defaultDateDate32"
-          allowCustomValue={false}
-          width={24}
-          value={jsonData.defaultDateDate32}
-          onChange={(changeEvent) => {
-            onFieldChange({ value: changeEvent ? changeEvent.value : undefined }, 'defaultDateDate32')
-          }}
-          options={defaultDateDate32Options}
-        />
-      </InlineField>
-    </>}
-  </div>
+            <Select
+              width={24}
+              onChange={(changeEvent) => {
+                onFieldChange({ value: changeEvent ? changeEvent.value : undefined }, 'defaultDateTimeType');
+              }}
+              isClearable
+              placeholder={'Timestamp type'}
+              options={[
+                { label: 'DateTime', value: TimestampFormat.DateTime },
+                { label: 'DateTime64', value: TimestampFormat.DateTime64 },
+                { label: 'TimeStamp', value: TimestampFormat.TimeStamp },
+              ]}
+              value={jsonData.defaultDateTimeType}
+            />
+          </InlineField>
+          <h6>DateTime columns</h6>
+          <InlineField label="Datetime Field" labelWidth={32} style={{ marginLeft: '30px' }}>
+            <Select
+              isClearable
+              id="defaultDateTime"
+              allowCustomValue={false}
+              width={24}
+              value={jsonData.defaultDateTime}
+              onChange={(changeEvent) => {
+                onFieldChange({ value: changeEvent ? changeEvent.value : undefined }, 'defaultDateTime');
+              }}
+              options={defaultDateTimeOptions}
+            />
+          </InlineField>
+          <InlineField label="Timestamp (Uint32) Field" labelWidth={32} style={{ marginLeft: '30px' }}>
+            <Select
+              isClearable
+              id="defaultUint32"
+              allowCustomValue={false}
+              width={24}
+              value={jsonData.defaultUint32}
+              onChange={(changeEvent) => {
+                onFieldChange({ value: changeEvent ? changeEvent.value : undefined }, 'defaultUint32');
+              }}
+              options={defaultUint32Options}
+            />
+          </InlineField>
+          <InlineField label="Datetime64 Field" labelWidth={32} style={{ marginLeft: '30px' }}>
+            <Select
+              isClearable
+              id="defaultDateTime64"
+              allowCustomValue={false}
+              width={24}
+              value={jsonData.defaultDateTime64}
+              onChange={(changeEvent) => {
+                onFieldChange({ value: changeEvent ? changeEvent.value : undefined }, 'defaultDateTime64');
+              }}
+              options={defaultDateTime64Options}
+            />
+          </InlineField>
+          <h6>Date column</h6>
+          <InlineField label="Date Field" labelWidth={32} style={{ marginLeft: '30px' }}>
+            <Select
+              isClearable
+              id="defaultDateDate32"
+              allowCustomValue={false}
+              width={24}
+              value={jsonData.defaultDateDate32}
+              onChange={(changeEvent) => {
+                onFieldChange({ value: changeEvent ? changeEvent.value : undefined }, 'defaultDateDate32');
+              }}
+              options={defaultDateDate32Options}
+            />
+          </InlineField>
+        </>
+      )}
+    </div>
+  );
 }

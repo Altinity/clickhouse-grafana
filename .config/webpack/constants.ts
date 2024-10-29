fecad27502fc2c19e79588fe38dedfdf981a0715
antip00 <antipov@altinity.com>
2024-10-29 19:06:52 +0300

    Update.

--- .config/webpack/constants.ts
+++ .config/webpack/constants.ts
@@ -1,2 +1,2 @@
-export const SOURCE_DIR = 'instrumented';
+export const SOURCE_DIR = 'src';
 export const DIST_DIR = 'dist';
--- webpack.config.ts
+++ webpack.config.ts
@@ -3,6 +3,29 @@ import { mergeWithRules } from 'webpack-merge';
 import grafanaConfig from './.config/webpack/webpack.config';
 import path from 'path';
 
+export const SOURCE_DIR = 'instrumented';
+
+export async function getEntries(): Promise<Record<string, string>> {
+  const pluginsJson = await glob('**/instrumented/**/plugin.json', { absolute: true });
+
+  const plugins = await Promise.all(pluginsJson.map((pluginJson) => {
+      const folder = path.dirname(pluginJson);
+      return glob(`${folder}/module.{ts,tsx,js,jsx}`, { absolute: true });
+    })
+  );
+
+  return plugins.reduce((result, modules) => {
+    return modules.reduce((result, module) => {
+      const pluginPath = path.dirname(module);
+      const pluginName = path.relative(process.cwd(), pluginPath).replace(/instrumented\/?/i, '');
+      const entryName = pluginName === '' ? 'module' : `${pluginName}/module`;
+
+      result[entryName] = module;
+      return result;
+    }, result);
+  }, {});
+}
+
 const config = async (env: any): Promise<Configuration> => {
   const baseConfig = await grafanaConfig(env);
   const customConfig = {
@@ -12,6 +35,7 @@ const config = async (env: any): Promise<Configuration> => {
           exclude: /(node_modules|v2)/,
           use: {
             options: {
+              configFile: "tsconfig_tests.json",
               jsc: {
                 baseUrl: './instrumented',
               },

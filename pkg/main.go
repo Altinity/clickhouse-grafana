32168a91dd0b7d32dbf0fbd8b9ccbff6ff502f30
Roman M <lunaticsugreen@gmail.com>
2026-03-31 02:42:53 +0200

    try to change Serve to Manage for BE plugin

--- pkg/main.go
+++ pkg/main.go
@@ -5,11 +5,29 @@ import (
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/datasource"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/tracing"
 )
+
+const pluginID = "vertamedia-clickhouse-datasource"
 
 func main() {
 	backend.Logger.Info("Starting ClickHouse datasource backend...")
-	err := datasource.Serve(GetDatasourceServeOpts())
+
+	// Setup plugin environment and tracing (required by Manage API)
+	backend.SetupPluginEnvironment(pluginID)
+	if err := backend.SetupTracer(pluginID, tracing.Opts{}); err != nil {
+		backend.Logger.Error("Failed to setup tracer", "error", err)
+	}
+
+	ds := &ClickHouseDatasource{
+		im: datasource.NewInstanceManager(NewDatasourceSettings),
+	}
+
+	err := backend.Manage(pluginID, backend.ServeOpts{
+		QueryDataHandler:    ds,
+		CheckHealthHandler:  ds,
+		CallResourceHandler: ds,
+	})
 
 	if err != nil {
 		backend.Logger.Error(err.Error())
